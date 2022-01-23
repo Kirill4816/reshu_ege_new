62d45ea127b307567851e96cc04fbafb37b52ef3
kirfa <akkaunt.1405@gmail.com>
2022-01-23 13:30:30 +0300

    доработки

--- variants_ege/v_1_yan/25.py
+++ variants_ege/v_1_yan/25.py
@@ -7,9 +7,13 @@ first_num = 35000000
 second_num = 40000000
 for number in range(first_num, second_num + 1):
     dividers = []
-    for div in range(1, number // 2 + 1):
-        if div % 2 != 0:
-            if number % div == 0:
+    for div in range(1, round(sqrt(number))):
+        if number % div == 0:
+            if div % 2:
                 dividers.append(div)
+            div_2 = number // div
+            if div_2 % 2 and div != div_2:
+                dividers.append(div_2)
     if len(dividers) == 5:
         print(number)
+        print(dividers)
--- variants_ege/v_1_yan/26/26.py
+++ variants_ege/v_1_yan/26/26.py
@@ -19,10 +19,10 @@ odd_numbers = [num for num in numbers if num % 2 == 0]
 len_numbers = len(odd_numbers)
 for i in range(len_numbers):
     for g in range(i + 1, len_numbers):
-        average_pair = odd_numbers[i] + odd_numbers[g] // 2
+        average_pair = (odd_numbers[i] + odd_numbers[g]) // 2
         if average_pair in numbers:
             print(average_pair)
             cnt_pair += 1
-            if average_pair // 2 > max_cnt_pair:
+            if average_pair > max_cnt_pair:
                 max_cnt_pair = average_pair
 print(cnt_pair, max_cnt_pair)
