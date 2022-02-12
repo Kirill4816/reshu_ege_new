c16c97cc8aee31003f08602e86e296c32f61d7bc
artem <zaitsev998@mail.ru>
2022-02-12 10:42:19 +0300

    24 done

--- variants_ege/v_2_feb/24/24.py
+++ variants_ege/v_2_feb/24/24.py
@@ -1,15 +1,23 @@
 with open('24 (4).txt', 'r') as file:
-    strings = file.readline().split()
+    strings = file.readlines()   # readline -> readlines, split don't used with arrays)
 
-min_cnt_N = 1000000
+min_cnt_N = float('inf')   # TODO remember this expression
 string_min_N = ''
-letters = {}
+letters = {}  # may be not used
 
 for string in strings:
     if string.count('N') < min_cnt_N:
         min_cnt_N = string.count('N')
         string_min_N = string
 
+most_freq_letter = ''
+freq_count = 0
 
 for i in range(ord('A'), ord('Z') + 1):
-    letters[chr(i)] = 0
+    letters[chr(i)] = 0   # ????  what is that
+    cnt = string_min_N.count(chr(i))
+    if cnt >= freq_count:  # >=   ->  the next letter is later in the alphabet, so it has priority
+        freq_count = cnt
+        most_freq_letter = chr(i)
+
+print(most_freq_letter)
